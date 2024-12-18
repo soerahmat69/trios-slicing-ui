9a73c5113e730966eac62770fae9ef478a08757a
soerahmat69 <surahmatr38@gmail.com>
2024-12-18 18:25:07 +0800

    update: ui section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <!-- <header>
     <nav>
       <ul>
         <li id="icon">
@@ -52,7 +52,7 @@
         </li>
       </ul>
     </nav>
-  </header>
+  </header> -->
 
   <main>
     <section id="hero">
@@ -60,17 +60,16 @@
       <h1 id="hero_text"><span>Trios</span>. Reliable solutions for 30 years</h1>
     </section>
 
-    <section class="container">
+    <section class="container bg-secondary">
       <div class="space-xl">
-        <article>
-          <p>
+        <article class="intro-text">
+          <div class="text_dot"></div>
+          <p class="t-white">
             For 30 years, our team has treated every project as if it were our own, with a great
             sense of responsibility. About the result, about your budget and your secrets. We
             realize what your company really needs and ensure that the solution stands like a stone
-          </p>
-        </article>
-        <article>
-          <p>
+            <br />
+            <br />
             We combine experience in various sectors and craftsmanship into customized solutions. In
             this, we go further than simply executing; we think along and always keep your interests
             in mind. That is where our strength lies. Trust in solutions that go beyond demand.
@@ -78,19 +77,317 @@
         </article>
       </div>
     </section>
+    <div class="swap">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 78"
+        fill="white"
+        preserveAspectRatio="xMidYMid meet"
+        style="display: block; line-height: 0"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M407.949 0C401.35 0 396 5.34991 396 11.9494V32C396 38.6274 390.627 44 384 44H0V78H396H412H1440V0H407.949Z"
+          fill="#ECF5FF"
+        ></path>
+      </svg>
+    </div>
+
+    <section class="section-mark container bg-primary">
+      <div class="space-xl">
+        <article class="intro-text">
+          <div class="text_dot"></div>
+          <p class="t-dark">
+            We zijn je productontwikkelaar, procesontwikkelaar en machinebouwer. We zijn het team
+            dat naast je staat bij elke stap en helpt. Met prototypes, duurtesten, CE- en
+            patentondersteuning, FEM berekeningen en projectmanagement.
+          </p>
+        </article>
+
+        <div class="mark-wrap">
+          <div class="mark-card-text">
+            <div class="mark-flex">
+              <p>Proptotypes</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+              >
+                <path
+                  d="M19 13.5L13 13.5L13 19.5L11 19.5L11 13.5L5 13.5L5 11.5L11 11.5L11 5.5L13 5.5L13 11.5L19 11.5L19 13.5Z"
+                  fill="#2B3467"
+                ></path>
+              </svg>
+            </div>
+            <p>
+              We build prototypes and zero series in our workshop. Here we test whether the design
+              does what it should do. High focus, speed and efficiency.
+            </p>
+          </div>
+          <div class="mark-image">
+            <img
+              src="./assets/images/674f19d0d40cd4619b25bbce_Trios-Brandshoot-Liike-2700px-0402913.jpg"
+              alt=""
+            />
+          </div>
+          <div class="mark-card-text">
+            <div class="mark-flex">
+              <p>FEM calculations</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+              >
+                <path
+                  d="M19 13.5L13 13.5L13 19.5L11 19.5L11 13.5L5 13.5L5 11.5L11 11.5L11 5.5L13 5.5L13 11.5L19 11.5L19 13.5Z"
+                  fill="#2B3467"
+                ></path>
+              </svg>
+            </div>
+            <p>
+              Build immediately? No. With FEM calculations, we foresee problems and see if we meet
+              the conditions.
+            </p>
+          </div>
+          <div class="mark-image">
+            <img
+              src="./assets/images/674f101f6504239e3308de24_Trios-Brandshoot-Liike-2700px-0402095.jpg"
+              alt=""
+            />
+          </div>
+          <div class="mark-card-text">
+            <div class="mark-flex">
+              <p>(Duration) tests</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+              >
+                <path
+                  d="M19 13.5L13 13.5L13 19.5L11 19.5L11 13.5L5 13.5L5 11.5L11 11.5L11 5.5L13 5.5L13 11.5L19 11.5L19 13.5Z"
+                  fill="#2B3467"
+                ></path>
+              </svg>
+            </div>
+            <p>
+              Prototypen, nulseries en onderdelen van machines controleren we zorgvuldig op
+              maatvoering, continuïteit en slijtage.
+            </p>
+          </div>
+          <div class="mark-image">
+            <img src="./assets/images/67592feec8d5860d47440b2a_Kern CNC klem.jpg" alt="" />
+          </div>
+
+          <div class="mark-card-text">
+            <div class="mark-flex">
+              <p>Project Management</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+              >
+                <path
+                  d="M19 13.5L13 13.5L13 19.5L11 19.5L11 13.5L5 13.5L5 11.5L11 11.5L11 5.5L13 5.5L13 11.5L19 11.5L19 13.5Z"
+                  fill="#2B3467"
+                ></path>
+              </svg>
+            </div>
+            <p>
+              We like to work together intensively and aim for long-term relationships in every
+              project. Good project management is part of that.
+            </p>
+          </div>
+          <div class="mark-image">
+            <img src="./assets/images/6737224a196dc6346f8ab027_Market Image-2.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container bg-primary">
+      <div class="space-xl">
+        <article class="intro-text">
+          <div class="text_dot"></div>
+          <p class="t-dark">Our professionals. The driving force behind our solutions</p>
+        </article>
+      </div>
+
+      <div class="team-grid">
+        <div class="team-card">
+          <div class="team-image">
+            <img src="./assets/images/67592feec8d5860d47440b2a_Kern CNC klem.jpg" alt="" />
+          </div>
+          <div class="team-card-text">
+            <h3>Mark van der Kelen</h3>
+          </div>
+        </div>
+
+        <div class="team-card">
+          <div class="team-image">
+            <img src="./assets/images/67592feec8d5860d47440b2a_Kern CNC klem.jpg" alt="" />
+          </div>
+          <div class="team-card-text">
+            <h3>Mark van der Kelen</h3>
+          </div>
+        </div>
+
+        <div class="team-card">
+          <div class="team-image">
+            <img src="./assets/images/67592feec8d5860d47440b2a_Kern CNC klem.jpg" alt="" />
+          </div>
+          <div class="team-card-text">
+            <h3>Mark van der Kelen</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="galery bg-primary space-xl">
+      <div class="galery-wrap">
+        <div class="item-galery">
+          <h3>
+            Trios is strong in conceptual thinking; dividing a big problem into bite-sized problems
+            and merging the sum of the small problems into a working whole.
+            <br />
+            <br />
+            De oplossingen zijn verrassend en altijd goed doordacht. Ideeën en concepten worden
+            intern besproken met meerdere disciplines voordat ze aan de klant worden gepresenteerd.
+            <br />
+            <br />
+            Tijdens de ontwikkeling van transportoplossingen wordt uitgebreid gerekend en
+            gesimuleerd. Dit proces geeft veel vertrouwen in de uiteindelijke resultaten.
+          </h3>
+        </div>
+        <div class="item-galery">
+          <h3>
+            Trios is strong in conceptual thinking; dividing a big problem into bite-sized problems
+            and merging the sum of the small problems into a working whole.
+          </h3>
+        </div>
+        <div class="item-galery">
+          <h3>
+            Trios is strong in conceptual thinking; dividing a big problem into bite-sized problems
+            and merging the sum of the small problems into a working whole.
+          </h3>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-primary desc-section">
+      <div class="space-xl desc-grid">
+        <div>
+          <h1 class="t-primary">Reliability at Trios: facts that speak</h1>
+          <p class="t-grey">
+            We focus on trust, innovation and craftsmanship. We'd love to tell you about that, but
+            the facts speak for themselves.
+          </p>
+        </div>
+        <div class="desc-data">
+          <div class="desc-item">
+            <h2 class="t-primary">30</h2>
+            <p>Years of experience</p>
+            <p class="t-grey">In precision engineering solution</p>
+          </div>
+          <div class="desc-item">
+            <h2 class="t-primary">30</h2>
+            <p class="t-dark">Years of experience</p>
+            <p class="t-grey">In precision engineering solution</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="vacature-section">
+      <div class="vacature-grid space-xl bg-primary">
+        <div class="vacature-item">
+          <img
+            src="./assets/images/6751cac1e85c1178d515e4e5_Trios-Portretten-Liike-2700px-0402051.jpg"
+            alt=""
+          />
+
+          <p class="t-grey">27 vacation days, and a well-arranged pension.</p>
+        </div>
+        <div class="vacature-item">
+          <img
+            src="./assets/images/674f101f47629be28a7a68cd_Trios-Brandshoot-Liike-2700px-0401540.jpg"
+            alt=""
+          />
+
+          <p class="t-grey">Daily coffee time at 10 a.m.</p>
+        </div>
+      </div>
+      <div class="vacature-info bg-primary">
+        <h5 class="t-primary">
+          Our people are our driving force. We value young talent and experienced professionals. We
+          are happy to invest in that. Also working for a company where your passion for the
+          profession is central and seen? We are always looking for more talent.
+        </h5>
+      </div>
+
+      <!-- Vacancies -->
+      <div class="space-xl bg-primary vacancies-detail">
+        <h2 class="t-primary">Vacancies</h2>
+        <div class="vacancies-detail-grid">
+          <div class="vacancies-detail-list">
+            <h3 class="t-primary">Toolmaker with CNC experience</h3>
+            <p class="t-grey">
+              Do you have an eye for detail, problem solving skills and, as a real professional, are
+              you always looking for quality? This toolmaker/instrument maker vacancy is for you.
+            </p>
+            <ul>
+              <li>
+                <div class="text_dot-list"></div>
+                <h4 class="t-primary">Neede</h4>
+              </li>
+              <li>
+                <div class="text_dot-list"></div>
+                <h4 class="t-primary">Full-Time</h4>
+              </li>
+
+              <li>
+                <div class="text_dot-list"></div>
+                <h4 class="t-primary">MBO</h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
 <style lang="sass">
+
+
+
+// global styles
 body
   margin: 0
   padding: 0
+  overflow-x: hidden
+
 
   a
     text-decoration: none
   li
     list-style: none
+  p
+    margin: 0
 
+  img
+    margin: 0
+    padding: 0
+
+  // header
 header
   position: fixed
   width: 100vw
@@ -110,7 +407,7 @@ header
       #icon
         font-size: 2.7rem
         letter-spacing: 0.1rem
-        font-family: 'PP Neue Montreal Bold'
+
       #menu
         padding: 10px 13px 10px 13px
         background-color: #2B3467
@@ -118,30 +415,236 @@ header
       li
         list-style: none
 
+
+.t-white
+  color: white
+
+.t-dark
+  color: black
+
+.t-primary
+  color:  #2B3467
+
+.t-grey
+  color:  #7c7f8d
+
+
+// main styles
+
 main
-  margin: 0
-  padding: 0
+
   width: 100vw
   height: auto
+  overflow: hidden
+
+  .vacature-section
+    width: 100%
+    display: grid
+    grid-template-columns: repeat(1, 1fr)
+
+    .vacature-info
+      padding: 0 4rem
+      h5
+        margin: 0
+        font-family: 'PP Neue Montreal Book'
+        font-size: 1.2rem
+    .vacancies-detail
+      height: auto
+      h2
+        margin: 1rem 0
+        font-family: 'PP Neue Montreal Book'
+        font-size: 1rem
+    .vacancies-detail-grid
+      display: grid
+      grid-template-columns: repeat(1, 1fr)
+      .vacancies-detail-list
+        background-color: white
+        padding: 1.5rem
+        h3
+          margin: 0
+          font-family: 'PP Neue Montreal Bold'
+          font-size: 1.5rem
+        p
+          margin: 0.5rem 0
+          font-family: 'PP Neue Montreal Book'
+          font-size: 1rem
+        ul
+          margin: 0
+          padding: 0
+          display: flex
+          gap: 1rem
+          li
+            display: flex
+            align-items: center
+            h4
+              text-indent: 0.7rem
+              margin: 0
+              font-family: 'PP Neue Montreal Book'
+              font-size: 1.2rem
+
+
+    .vacature-grid
+      display: grid
+      grid-template-columns: repeat(1, 1fr)
+
+      .vacature-item
+        img
+          object-fit: cover
+          width: 100%
+        p
+          margin: 0
+          padding: 1rem 0
+          font-family: 'PP Neue Montreal Book'
+          font-size: 1.2rem
+
+
+  .desc-section
+    .desc-grid
+      display: grid
+      grid-template-columns: repeat(1, 1fr)
+      gap: 2rem
+    .desc-data
+      display: grid
+      grid-template-columns: repeat(2, 1fr)
+      gap: 1rem
+      .desc-item
+        background-color: white
+        padding: 1rem
+        font-family: 'PP Neue Montreal Book'
+        h2
+          margin: 0
+          font-size: 2.7rem
+        p
+          margin: 0.2rem 0
+          font-size: 1rem
+    p
+      margin-top: 1rem
+      font-family: 'PP Neue Montreal Book'
+      font-size: 1.2rem
+    h1
+      margin: 0
+      font-family: 'PP Neue Montreal Book'
+      font-size: 2.7rem
+
+  .galery
+    width: 100%
+    overflow-x: scroll
+    .galery-wrap
+      width: 200vw
+      position: sticky
+      height: 100%
+      border: 1px solid red
+      display: flex
+      gap: 1rem
+      flex-wrap: nowrap
+
+    .item-galery
+      border: 1px solid green
+      height: 80vh
+      display: flex
+      align-items: center
+      padding: 0 2.7rem
+      h3
+        text-align: center
+        font-family: 'PP Neue Montreal Bold'
+        font-size: 1.5rem
+
+
+
+
+
+  .team-grid
+    padding: 0 4rem
+    display: grid
+    grid-template-columns: repeat(2, 1fr)
+    gap: 1rem
+
+
+
+
+  .team-card
+    display: flex
+    flex-direction: column
+    padding: 1rem
+    width: auto
+    height: auto
+    background-color: white
+    h3
+      margin: 0
+    img
+      // margin-top: 1rem
+      aspect-ratio: 1
+      width: 100%
+      object-fit: cover
+
+    .team-card-text
+      padding-top: 0.2rem
+      font-family: 'PP Neue Montreal Book'
+      font-size: 1rem
+      color: #2B3467
+
+  .mark-flex
+    display: flex
+    align-items: center
+    gap: 1rem
 
   .container
     width: 100%
     height: auto
-    max-width: 1200px
-    background-color: #2b3467
-  .space-xl
-    padding: 5rem
 
-  article
-    color: white
-    padding: 0px 1rem
+
+  .space-xl
+    padding: 4rem
+
+  .bg-secondary
+    background-color: #2b3467
+
+  .bg-primary
+    background-color: #ECF5FF
+
+  .intro-text
+    text-indent: 1.5rem
     font-family: 'PP Neue Montreal Book'
     font-size: 1.25rem
+    position: relative
+
+  .mark-card-text
+    font-family: 'PP Neue Montreal Book'
+    font-size: 1.25rem
+    padding: 5rem 2rem 0px 2rem
+    color: #2B3467
+    p
+      margin: 0.5rem 0
+
+
+  .mark-image
+    width: 100%
+    height: 20%
+
+    img
+      margin-top: 1rem
+      width: 100%
+      height: 10rem
+      object-fit: cover
+
+  .mark-wrap
+    flex-flow: column
+    display: flex
+    background-color: white
+
+    margin-top: 2rem
+
+  .swap
+    margin-top: -3rem
+
+
+
+
 
   #hero
     height: 100vh
     width: 100%
-    background-image: url('./assets/Trios_assets/hero_bg.jpg')
+    background-image: url('./assets/images/hero_bg.jpg')
     background-repeat: no-repeat
     background-size: cover
 
@@ -154,11 +657,26 @@ main
 
     #hero_text
       font-family: 'PP Neue Montreal Book'
+      letter-spacing: 0.3rem
+
       font-size: 3.5rem
       color: white
       left: 4%
-      bottom: 2%
+      bottom: 6%
       position: absolute
       span
-        color: red
+        color: #eb455f
+
+.text_dot
+  background-color: #eb455f
+  width: .375rem
+  height: .375rem
+  position: absolute
+  top: 0.5rem
+
+.text_dot-list
+  background-color: #eb455f
+  width: .375rem
+  height: .375rem
+  top: 0.5rem
 </style>
